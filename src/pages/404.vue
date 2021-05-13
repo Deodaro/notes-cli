911e474c60d1268ac9c894dc81dfdaafb2345e6d
Deodaro <l_r@list.ru>
2021-05-13 21:36:09 +1000

    Init

---
+++ src/pages/404.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+        <h1>Page not found.</h1>
+        <p>Go to <router-link class="link" to="/">main page?</router-link></p>
+      </div>
+    </section>
+  </div>
+</template>
